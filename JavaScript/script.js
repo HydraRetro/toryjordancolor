ed5afa779b60f4d84311e513b5b3713f8c1ff4f8
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-04-06 02:45:12 -0700

    Added code for video gallery

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,13 +9,49 @@ toggleButton.onclick = function () {
 }
 
 // VIDEO GALLERY
-// document.querySelectorAll('.videoContainer video').forEach(vid => {
-//     vid.onclick = () => {
-//         document.querySelector('.popupVideo').style.display = "block";
-//         document.querySelector('.popupVideo video').src = vid.getAttribute('src');
-//     }
-// })
+const videos = document.querySelectorAll('.video');
+const lightbox = document.querySelector('.lightbox');
+const lightboxVideo = document.querySelector('.lightbox-video iframe');
+const closeButton = document.querySelector('.close-button');
 
-// document.querySelector('.popupVideo span').onclick = () => {
-//     document.querySelector('.popupVideo').style.display = "none";
-// }
+videos.forEach(video => {
+    const id = video.getAttribute('data-id');
+    const thumbnail = video.querySelector('img');
+    const playButton = video.querySelector('.play-button');
+
+    video.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxVideo.setAttribute('src', `https://www.youtube.com/embed/${id}?autoplay=1`);
+    });
+
+    closeButton.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+        lightboxVideo.setAttribute('src', '');
+    });
+
+    lightbox.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+        lightboxVideo.setAttribute('src', '');
+    });
+
+    // Show play button on hover
+    video.addEventListener('mouseenter', () => {
+        playButton.style.display = 'block';
+    });
+
+    video.addEventListener('mouseleave', () => {
+        playButton.style.display = 'none';
+    });
+
+    // Hide play button on click and show video
+    playButton.addEventListener('click', event => {
+        event.stopPropagation();
+        video.click();
+    });
+});
+
+
+
+
+
+
