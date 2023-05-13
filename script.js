77687cb798e28e21c7a2c8cb2b3cf4093834b41b
HydraRetro <119288194+HydraRetro@users.noreply.github.com>
2023-05-13 00:11:00 -0700

    Removed from folders

---
+++ script.js
@@ -0,0 +1,71 @@
+// FILM STILLS
+const galleryImgs = document.querySelectorAll('.stillsGallery-img');
+const stillsLightbox = document.querySelector('.stillsLightbox');
+const lightboxImg = document.querySelector('.stillsLightbox-img');
+const closeBtn = document.querySelector('.stillsClose');
+
+galleryImgs.forEach((img) => {
+    img.addEventListener('click', () => {
+        stillsLightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    stillsLightbox.style.display = 'none';
+});
+
+stillsLightbox.addEventListener('click', (event) => {
+    if (event.target === stillsLightbox || event.target === lightboxImg) {
+        stillsLightbox.style.display = 'none';
+    }
+});
+
+// VIDEO GALLERY
+const videos = document.querySelectorAll('.video');
+const lightbox = document.querySelector('.lightbox');
+const lightboxVideo = document.querySelector('.lightbox-video iframe');
+const closeButton = document.querySelector('.close-button');
+
+videos.forEach(video => {
+    const id = video.getAttribute('data-id');
+    const thumbnail = video.querySelector('img');
+
+    video.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxVideo.setAttribute('src', `https://www.youtube.com/embed/${id}?autoplay=0`);
+    });
+
+    closeButton.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+        lightboxVideo.setAttribute('src', '');
+    });
+
+    lightbox.addEventListener('click', () => {
+        lightbox.style.display = 'none';
+        lightboxVideo.setAttribute('src', '');
+    });
+});
+
+// BACK TO TOP
+const topButton = document.getElementById("topBtn");
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+};
+
+const topFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+
+
+
+
+
+
